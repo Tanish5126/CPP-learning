d1d2567fe8029ff6eab8097ad13bc2e05aac59bd
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-06 15:37:44 +0000

    first day of learning c++

---
+++ C++_basics/01_basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // standard c++ library header for input and output operations
+using namespace std; //C++ statement that lets you use standard library names without writing std:: every time.
+
+int main() {     //starting point of a C++ program.
+    cout << "Hello World" << endl;
+    return 0;
+
+}
+
---
+++ C++_basics/02_basics.cpp
@@ -0,0 +1,15 @@
+// Working with variables
+# include <iostream>
+using namespace std;
+
+int main() {
+    int a = 78;   
+    int b = 67;
+    string name = "Tanish";
+
+    cout<<"Sum of a and b is:"<< a+b<< endl;
+    cout<<"My name is"<< name;
+    
+    return 0;
+
+}
