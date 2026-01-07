512dd5dac98322e21563b317449e52e0da3a6e69
Tanish Gawade <2025.tanishg@isu.ac.in>
2026-01-07 15:16:40 +0000

    Learnt about data types and input/output operations

---
+++ C++_basics/03_basics.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+using namespace std;
+
+
+int glo = 20;
+void sum() {
+    int a;
+    cout << glo;
+    }
+    
+
+
+int main() {
+    int glo = 9;
+    glo = 18;
+
+    int a=14, b=15; 
+    float pib= 3.14; 
+    char c ='d';
+
+    bool is_true = true;   //true value will print '1' at the end; false will print '0'
+    sum();
+    cout << glo<<is_true;
+    
+    
+    
+    return 0 ;
+}
---
+++ C++_basics/04_basics.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+    int num1 , num2;
+    cout<<"Enter the first number:";    // '<<' is insertion operator
+    cin >> num1;                             // '>>' is extraction operator
+
+    cout<<"Enter the second number:";
+    cin >> num2;
+    
+    cout<<"Sum of both numbers is:"<<num1 + num2;
+
+}
